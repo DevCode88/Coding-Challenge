1f54f336bfef5809ad307fdcdb0ba6cd2fb9d632
Naveen Suthar <136985202+DevCode88@users.noreply.github.com>
2025-02-20 19:11:51 +0530

    Create Q1_Check_odd_or_even.cpp

---
+++ Bit_Manipulation/Q1_Check_odd_or_even.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int n = 50;
+
+    if(n&1){
+
+        cout<<"Odd number";
+
+    }
+
+    else{
+
+        cout<<"Even number";
+
+    }
+
+    return 0;
+
+}
