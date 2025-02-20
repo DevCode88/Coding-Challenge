835f5e42482edb2aac1d135d9971e2e4d8e8a5ad
Naveen Suthar <136985202+DevCode88@users.noreply.github.com>
2025-02-20 19:13:12 +0530

    Create Q4_Clear_ith_bit.cpp

---
+++ Bit_Manipulation/Q4_Clear_ith_bit.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    int n = 10;
+
+    int i = 1;
+
+    int mask = ~(1<<i);
+
+    int ans = n & mask;
+
+    cout<<ans;
+
+    return 0;
+
+}
